a321371bcda870bfa5856ea3b0a430e44418e297
Keith Tan <samkeithtan@me.com>
2017-04-23 10:34:34 +0800

    Initial Commit

---
+++ Iris Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Iris Manager
+//
+//  Created by Keith Tan on 4/23/17.
+//  Copyright © 2017 Axis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
